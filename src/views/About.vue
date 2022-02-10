f69e57c16ade06f3e62e52a4b56b08a6d48fa950
Pattaravadee2407 <junthamanee.ice@gmail.com>
2022-02-11 02:56:22 +0700

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,8 +3,8 @@
     <br>
     <center>
     <!--  <h1>This is an about page.</h1>  -->
-    <h1>PATTARAVADEE</h1>
-    <h3>6304101355</h3>
+    <h1>Ditchaphong</h1>
+    <h3>6304101327</h3>
     </center>
     
   </div>
